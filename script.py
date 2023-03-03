3f3398dd578b1bc31d7b0e776590c15837f43b99
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-03-03 11:14:30 +0100

    add_data to csv file

--- script.py
+++ script.py
@@ -6,7 +6,8 @@ datas = []
 with open('datas.csv', 'r') as csvfile:
     try:
         spamreader = csv.reader(csvfile)
-        next(spamreader)
+
+        next(spamreader, None)
 
         for row in spamreader:
             horodatage = datetime.fromisoformat(row[0])
@@ -20,4 +21,38 @@ with open('datas.csv', 'r') as csvfile:
     except IOError:
         exit()
 
-print(datas)
+
+def add_data(temperature: float, humidite: float):
+    horodatage = datetime.now()
+
+    if len(datas) == 30:
+        datas.pop(0)
+
+    datas.append({
+        'horodatage': horodatage,
+        'temperature': temperature,
+        'humidite': humidite
+    })
+
+    try:
+        with open('datas.csv', 'w+') as csvfile:
+            csvwriter = csv.writer(csvfile)
+
+            csvwriter.writerow(['horodatage', 'temperature', 'humidite'])
+
+            for i in range(len(datas)):
+                list_values = list(datas[i].values())
+
+                horodatage = list_values[0].strftime(
+                    "%Y:%m:%d %H-%M-%S")
+                temperature = float(list_values[1])
+                humidite = float(list_values[2])
+
+                csvwriter.writerow([horodatage, temperature, humidite])
+
+    except IOError:
+        exit()
+
+
+for _ in range(50):
+    add_data(30, 30)
